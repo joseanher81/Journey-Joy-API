816fb743e8c6efdf2bb9b127a1f8a3c2d8942148
Jose Angel Hernandez <joseanher81@gmail.com>
2024-07-15 16:10:10 +0200

    added json middleware to server

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const moongose = require('mongoose');
 const app = express();
 
 // midlewares
+app.use(express.json());
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+});
 
 // routes
 
